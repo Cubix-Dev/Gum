868754daf39dbb5c6f1d4ab119b04797781c3769
Ship <silverteamroblox@gmail.com>
2023-05-12 19:23:16 -0400

    BASIC ast done

---
+++ ast.ts
@@ -0,0 +1,40 @@
+export type NodeType = 
+    | "Program" 
+    | "NumericLiteral" 
+    | "Indentifier" 
+    | "BinaryExpr";
+
+// This will not return a value, thats what our expressions are for
+export interface statement {
+    kind: NodeType;
+}
+
+// Finally! AP Computer Science A coming in clutch!
+export interface Program extends statement {
+    kind: "Program"
+    body: statement[]
+}
+
+// This is for stuff that will return values. If it doesn't return a value its a statment
+// local mii = 1 is a statement
+// mii = 1 is an expression
+export interface Expr extends statement {}
+
+// math basically
+// 10 - 5, foo + bar, etc
+export interface BinaryExpr extends Expr {
+    kind: "BinaryExpr"
+    left: Expr
+    right: Expr
+    operator: string
+}
+
+export interface Indentifier extends Expr {
+    kind: "Indentifier"
+    symbol: string,
+}
+
+export interface NumericLiteral extends Expr {
+    kind: "NumericLiteral"
+    value: number,
+}
